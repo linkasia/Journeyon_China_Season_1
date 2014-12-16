969f0c5a5cf6bf1cca8f9814ec4bcdd35169bb26
sigurae723 <sbs@linkasia.co.kr>
2014-12-16 17:36:34 +0900

    githubTest
    깃허브 테스트 박스만듬, 이미지 넣어봄

--- application/views/mypage/guestCancel.php
+++ application/views/mypage/guestCancel.php
@@ -2,5 +2,8 @@
 			<div class="title3">
 				<div class="underline">客户 - 取消</div>
 			</div>
+			<div class="githubTest">
+				<img src="/application/views/images/mypage/1394446070_firefox.png" alt="">
+			</div>
 
 		</div><!-- hostWrap 닫음 -->
