31d0421dc228a8f0e41022490cb0c07625725fcd
hyunchoul0727 <hhc@linkasia.co.kr>
2014-12-24 09:53:51 +0900

    page
    page compenent

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -85,7 +85,7 @@ $maincontents="当地人推荐的最佳自由行之地 Journey On";
 									<img src="/application/views/images/main/profile01.jpg" alt="myprofile_Picture" class="img-circle" type="button" id="profileMenu" data-toggle="dropdown" aria-expanded="true"><span class="caret caret-block" data-toggle="dropdown" aria-expanded="true"></span>
 								<!-- </button> -->
 								<ul class="dropdown-menu" role="menu" aria-labelledby="profileMenu">
-									<li role="presentation"><a role="menuitem" tabindex="-1" href="#">View on Public</a></li>
+									<li role="presentation"><a role="menuitem" tabindex="-1" href="/index.php/mypage/myPage_M/mypageintro">View on Public</a></li>
 									<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Account&Setting</a></li>
 									<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Guide</a></li>
 									<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Guest</a></li>
