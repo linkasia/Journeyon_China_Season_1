72afa99dbae7ba8fdd55a3050fa9503317b93ef0
sigurae723 <sbs@linkasia.co.kr>
2014-12-24 17:51:32 +0900

    12/24 Mypage work
    Mypage
    메뉴, 필요한 페이지 새로 작성
    팝업에 필요한 CSS작�

--- application/views/mypage/menu_account.php
+++ application/views/mypage/menu_account.php
@@ -1,2 +1,141 @@
-		<div class="profileWrap1">
-		</div><!-- profileWrap1 -->
+<div id="accountWrap">
+	<aside id="leftmenuWrap">
+		<span class="list-group-item active">Account & Setting&nbsp;&nbsp;<span class="glyphicon glyphicon-cog" aria-hidden="true"></span></span>
+		<span class="list-group-item" id='Account'><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;Account</span>
+		<span class="list-group-item" id='Payment'><span class="glyphicon glyphicon-usd" aria-hidden="true"></span>&nbsp;&nbsp;Payment&nbsp;&nbsp;</span>
+		<span class="list-group-item" id='V Certicification'><span class="glyphicon glyphicon-pushpin" aria-hidden="true"></span>&nbsp;&nbsp;V Certicification&nbsp;&nbsp;</span>
+	</aside><!-- leftmenuWrap end -->
+
+	<div id="rightWrap">
+		<div class="accountDetail">
+			<p>Account Detail</p>
+			<div class="email">
+				<span class="email-f1">E-mail</span>
+				<span class="email-f2">james@linkasia.co.kr</span>
+				<span class="email-f3"><a href="#" data-toggle="modal" data-target="#myModal1">Change your E-Mail</a></span>
+
+					<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+									<h4 class="modal-title" id="myModalLabel">Email Change</h4>
+								</div><!-- modal-header 닫힘 -->
+								<div class="modal-body">
+									  <div id='content'>
+											<span>Present E-mail</span><span>James@linkasia.co.kr</span><br>
+											<span>New E-Mail</span><input type="textbox"><br>
+											<span>Password</span><input type="password">
+									  </div>
+								</div><!-- modal-body 닫힘 -->
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary">Confirm New E-mail</button>
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div><!-- modal-footer 닫힘 -->
+							</div><!-- modal-content 닫힘 -->
+						</div><!-- modal-dialog 닫힘 -->
+					</div><!-- modal fade Overveiw 끝 -->
+
+			</div><!-- email end -->
+			<div class="password">
+				<span>Password</span>
+				<span><a href="#" data-toggle="modal" data-target="#myModal2">Change your password</a></span>
+
+					<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+									<h4 class="modal-title" id="myModalLabel">Password Change</h4>
+								</div><!-- modal-header 닫힘 -->
+								<div class="modal-body">
+									  <div id='content'>
+											<span>Present Password</span><input type="password"><br>
+											<span>New Password</span><input type="password"><br>
+											<span>Password</span><input type="password">
+									  </div>
+								</div><!-- modal-body 닫힘 -->
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary">Confirm</button>
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div><!-- modal-footer 닫힘 -->
+							</div><!-- modal-content 닫힘 -->
+						</div><!-- modal-dialog 닫힘 -->
+					</div><!-- modal fade Overveiw 끝 -->
+
+			</div><!-- password end -->
+		</div><!-- accountDetail end -->
+		<div class="basicInformation">
+			<p>Basic Information</p>
+			<div class="grew">
+				<span>Grew up in</span>
+
+				<!-- Split button -->
+				<div class="btn-group">
+					<button type="button" class="btn btn-info">Country</button>
+					<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					<span class="caret"></span>
+					<span class="sr-only">Toggle Dropdown</span>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">China</a></li>
+						<li><a href="#">U.S.A</a></li>
+						<li><a href="#">Korea</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Japan</a></li>
+						<li><a href="#">France</a></li>
+						<li><a href="#">Germen</a></li>
+						<li><a href="#">India</a></li>
+						<li><a href="#">Taiwan</a></li>
+					</ul>
+				</div><!-- Split button -->
+
+				<!-- Split button -->
+				<div class="btn-group">
+					<button type="button" class="btn btn-info">City</button>
+					<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					<span class="caret"></span>
+					<span class="sr-only">Toggle Dropdown</span>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">China</a></li>
+						<li><a href="#">U.S.A</a></li>
+						<li><a href="#">Korea</a></li>
+						<li><a href="#">Japan</a></li>
+						<li><a href="#">France</a></li>
+						<li><a href="#">Germen</a></li>
+						<li><a href="#">India</a></li>
+						<li><a href="#">Taiwan</a></li>
+					</ul>
+				</div><!-- Split button -->
+			</div>
+
+		</div>
+		<button class="btn btn-primary savebtn">Save changes</button>
+		<a href="#" class="deactive" data-toggle="modal" data-target="#myModal3">Deactivate your</a>
+			<div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+							<h4 class="modal-title" id="myModalLabel">Confirmation</h4>
+						</div><!-- modal-header 닫힘 -->
+						<div class="modal-body">
+							  <div id='content'>
+									<p>한번 비활성화되면 다시 살릴 수 없습니다. 개인정보를 제외한 나머지 정보들은 사이트에 지속 남아있습니다.
+									정말로 지우겠습니까?<br>
+									<button class="btn btn-danger">No, 아니 다시 생각해보니 안지울래요.</button><br>
+									<button class="btn btn-info">Yes, 다음에 좋은 기회에 다시 봐요.</button>
+									</p>
+							  </div>
+						</div><!-- modal-body 닫힘 -->
+						<div class="modal-footer">
+							<button type="button" class="btn btn-primary">Confirm</button>
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						</div><!-- modal-footer 닫힘 -->
+					</div><!-- modal-content 닫힘 -->
+				</div><!-- modal-dialog 닫힘 -->
+			</div><!-- modal fade Overveiw 끝 -->
+
+	</div><!-- rightWrap end -->
+</div><!-- accountWrap end -->
