77599268689ceb87f2313b5f2100c73db9deff5d
sigurae723 <sbs@linkasia.co.kr>
2014-12-24 11:02:25 +0900

    New PHP file create
    New PHP file create

---
+++ application/views/mypage/m_guest.php
@@ -0,0 +1,20 @@
+		<div class="profileWrap1">
+			<div class="title1">
+				<div class="underline">基本信息</div>
+			</div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
+			<div class="profile_text1">是否是专业导游<br>Host<br>生活在<br>现居住在纽约<br>年龄<br>性别<br>注册时间<br>语言<br>职业<br>最近登录时间<br></div>
+			<div class="profile_text2">是<br>Available<br>韩国<br>5年<br>30岁<br>女<br>2014年01月02日<br>韩国语（上）汉语（中）<br>学生、专业<br>10分钟前（微博为准）<br>
+				<div class="sns_icon">
+					<img src="/application/views/images/main/sns01.png" alt="">
+					<img src="/application/views/images/main/sns02.png" alt="">
+					<img src="/application/views/images/main/sns03.png" alt="">
+					<img src="/application/views/images/main/sns04.png" alt="">
+					<img src="/application/views/images/main/sns05.png" alt="">
+					<img src="/application/views/images/main/sns06.png" alt="">
+					<img src="/application/views/images/main/sns07.png" alt="">
+					<img src="/application/views/images/main/sns08.png" alt="">
+				</div>
+			</div>
+			<img src="/application/views/images/main/profile01.jpg"  class="profilePic img-circle" alt="profile_picture">
+			<p class="btn btn-danger edit_profile1" id='profileModify'>Modify</p>
+		</div><!-- profileWrap1 -->
