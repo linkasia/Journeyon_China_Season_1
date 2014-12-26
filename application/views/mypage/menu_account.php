716feaa3bd9e0e76f176c781e867bb2e352c7c96
sigurae723 <sbs@linkasia.co.kr>
2014-12-26 14:59:28 +0900

    simplemodal.less
    냄

--- application/views/mypage/menu_account.php
+++ application/views/mypage/menu_account.php
@@ -73,12 +73,12 @@
 				<div class="button1">
 					<!-- Split button -->
 					<div class="btn-group">
-						<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown1" aria-expanded="false">Country</button>
-						<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown1" aria-expanded="false">
+						<input type="button" class="btn btn-info dropdown-toggle" value="Country" data-toggle="dropdown" aria-expanded="false">
+						<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
 						<span class="caret"></span>
 						<span class="sr-only">Toggle Dropdown</span>
 						</button>
-						<ul class="dropdown1-menu" role="menu">
+						<ul class="dropdown-menu" role="menu">
 							<li><a href="#">China</a></li>
 							<li><a href="#">U.S.A</a></li>
 							<li><a href="#">Korea</a></li>
