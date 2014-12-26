40478037c7e6906440b282c47b32d169bfbdaeec
sigurae723 <sbs@linkasia.co.kr>
2014-12-26 13:49:34 +0900

    12/26 Header js loading

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -55,7 +55,7 @@ $maincontents="当地人推荐的最佳自由行之地 Journey On";
 		<script type="text/javascript" src="/application/views/jqwidgets/globalization/globalize.js"></script>
 
 
-		<link rel="stylesheet" href="simplemodal.css" type="text/css" media="screen" title="no title" charset="utf-8">
+		<link rel="stylesheet" href="/application/views/css/simplemodal.css" type="text/css" media="screen" title="no title" charset="utf-8">
 		<script src="/application/views/js/mootools-core-1.3.1.js" type="text/javascript" charset="utf-8"></script>
 		<script src="/application/views/js/mootools-more-1.3.1.1.js" type="text/javascript" charset="utf-8"></script>
 		<script src="/application/views/js/simple-modal.js" type="text/javascript" charset="utf-8"></script> 
