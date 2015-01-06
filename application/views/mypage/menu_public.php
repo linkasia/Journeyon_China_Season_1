7db57f51f75886945185ef46107777b4c6224d2f
sigurae723 <sbs@linkasia.co.kr>
2015-01-06 09:22:41 +0900

    Sync
    아침 싱크

--- application/views/mypage/menu_public.php
+++ application/views/mypage/menu_public.php
@@ -1,3 +1,15 @@
-		<div class="profileWrap1">
-		123123
-		</div><!-- profileWrap1 -->
+<div id="mypublicWrap">
+
+	<!-- 왼쪽 프로필 사진 영역 -->
+	<aside id="leftAside">
+		
+	</aside>
+
+
+	
+	<!-- 여기서부터 오른쪽 박스 -->
+	<div id="rightDiv">
+		<div id="introduceSelf"></div>
+		<div id="profileQuestion"></div>
+	</div>
+</div><!-- mypublicWrap -->
