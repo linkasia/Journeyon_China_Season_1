bc93c00907f07c17cf533c4ac290e58a59fc8f98
sigurae723 <sbs@linkasia.co.kr>
2015-01-09 16:16:55 +0900

    2015.1.9
    Guide - Product management 작업중

--- application/views/mypage/m_guide2.php
+++ application/views/mypage/m_guide2.php
@@ -1,5 +1,19 @@
 		<div class="profileWrap1">
-			<div class="title1">
-				<div class="underline">상품관리</div>
+			<img src="/application/views/images/mypage/img_guide1.png" alt="guide_image">
+			<div class="guideimgBand">
+				<span class="guide_txt">ㅇㅇㅇ님 당신의 경험을 등록해보세요~~~!</span>
+				<span class="guide_next">
+				<span class="glyphicon glyphicon-chevron-right"></span>  로컬 가이드 알아보러가기</span>
+			</div>
+			<div class="title2">
+				<div class="underline">Product Management</div>
 			</div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
+			<div class="pmWrap">
+			<a href="/index.php/City/country/productUpload" class="btn btn-info product-upload" id='productUpload' name='productUpload'>Guide 상품 올리러가기</a>
+			<ul class="nav nav-tabs">
+			  <li role="presentation" class="active"><a href="">판매중</a></li>
+			  <li role="presentation"><a href="">등록중</a></li>
+			  <li role="presentation"><a href="">판매중지</a></li>
+			</ul>
+			</div>
 		</div><!-- profileWrap1 -->
