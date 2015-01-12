7499e24ea096406128a74ab48b30fc95d107b981
sigurae723 <sbs@linkasia.co.kr>
2015-01-12 18:19:30 +0900

    2015.01.12
    Guide - Product Management CSS

--- application/views/mypage/m_guide2.php
+++ application/views/mypage/m_guide2.php
@@ -6,176 +6,23 @@
 					<span class="glyphicon glyphicon-chevron-right"></span>  로컬 가이드 알아보러가기
 				</span>
 			</div>
+
 			<div class="title2">
 				<div class="underline">Product Management</div>
 			</div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
 
+
 			<div class="pmWrap">
-				<a href="/index.php/City/country/productUpload" class="btn btn-info product-upload" id='productUpload' name='productUpload'>Guide 상품 올리러가기</a>
+				<a href="/index.php/City/country/productUpload" class="btn btn-primary product-upload" id='productUpload' name='productUpload'>Guide 상품 올리러가기</a>
 				<ul class="nav nav-tabs">
 				  <li role="presentation" class="active"><a href="">판매중</a></li>
 				  <li role="presentation"><a href="">등록중</a></li>
-				  <li role="presentation"><a href="">판매중지</a></li>
+				  <li role="presentation"><a href="">Stop</a></li>
 				</ul>
 				
-				<!-- 여기서부터 탭 영역 시작 Page -->
-				<div class="btn-group sortGroup" role="group" aria-label="" id="sortGroup">
-					<button type="button" class="btn btn-primary" id="sort1" name="sort1">등록순</button>
-					<button type="button" class="btn btn-primary" id="sort2" name="sort2">후기순</button>
-					<button type="button" class="btn btn-primary" id="sort3" name="sort3">판매순</button>
-					<button type="button" class="btn btn-primary" id="sort4" name="sort4">좋아요순</button>
-				</div>
-				<div></div>
-				<!-- 판매중탭 끝 -->
+				<?php $this -> load ->view('/mypage/m_guide2_tab1'); ?><!-- Tab영역 불러오기 -->
+				<?php $this -> load ->view('/mypage/m_guide2_tab2'); ?><!-- Tab영역 불러오기 -->				
+				<?php $this -> load ->view('/mypage/m_guide2_tab3'); ?><!-- Tab영역 불러오기 -->	
 
-				<!-- 여기서부터 섬네일 시작 -->
-				<div class="contents_list1">
-					<div class="list_image1">
-						<div class="statusBand transblack">
-							<p id="status" name="status">2015.1.12 판매시작<br>
-							<span class="sub_title"><span id="pNumber">160</span>명의 고객이 다녀갔습니다.</span>
-							</p>
-						</div>
-						<div class="band">
-							<div class="wishcount">
-								<img src="/application/views/images/main/img23_heart.PNG" alt="" class="heart_image">
-								<span id="heart" name="heart"> 15</span> 
-							</div>
-							<div class="reviewcount">
-								<img src="/application/views/images/main/img24_talkbox.PNG" alt="" class="talkbox_image">
-								<span id="talkbox" name="talkbox"> 11</span>
-							</div>
-							<div class="localcount">
-								<img src="/application/views/images/main/img27_navi.PNG" alt="" class="navi_image">
-								<span id="talkbox" name="talkbox"> 当地人</span>
-							</div>
-						</div>
-						<img src="/application/views/images/contents/img01.jpg" alt="" class="pic1_img" style="width: 100%; height: 100%;">
-						<img src="/application/views/images/main/profile01.jpg" alt="" class="img-circle profile_image">
-						<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
-						<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
-					</div><!-- list_image1 닫힘 -->
-					<div class="list_rightwrap whitegloss">
-						<div class="right_title"><span>纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题</span>
-						</div>
-						<div class="money greybluegloss"><span>500元</span></div>
-						<div class="right_text">纽约是什么样的呢？是电影中午夜时分的灯红酒绿，还是黑暗阴影中的危机四伏？每个人心目中都有一副纽约的图像，诚然，纽约是一座集经济、科技、旅游、娱乐、影视、时尚于一身的豪华大城市。这里是旅游之都，高耸的帝国大厦，散发着耀眼的光芒， 就如同霸气一身的王者；被誉为“照耀世界之神”的自由女神像，彰显着美丽的容颜和非凡的气质，令无数的游客折服，每一个到美国的人都不会错过。这里也是购物之都，人潮拥挤的第五大道，是购物狂的天堂，你几乎可以买到世界上任何的名牌。这里还是经济之都，在气氛紧张的纽约证劵交易所里，每天都上演着经济大战......不要只停留在美剧当中的纽约啦，赶快出发吧，玩转纽约，来一场说走就走的旅行吧！ 
-						</div>
-						<div class="bottom_text">
-							<div class="line1">
-								<div class="tag"><img src="/application/views/images/left_menu/icon11_adventure.PNG" alt="">冒险</div>
-								<div class="tag"><img src="/application/views/images/left_menu/icon12_relaxation.PNG" alt="">休闲</div>
-								<div class="tag"><img src="/application/views/images/left_menu/icon13_romantic.PNG" alt="">浪漫</div>
-							</div>
-							<div class="line2">
-								<div class="tag2"><img src="/application/views/images/left_menu/icon21_solo.PNG" alt="">单人</div>
-								<div class="tag2"><img src="/application/views/images/left_menu/icon23_family.PNG" alt="">家族</div>
-							</div>
-						</div> <!-- bottom_text 닫음 -->
-					</div> <!-- list_rightwrap 닫음 -->
-				</div><!-- contents_list1 닫음 -->
-				<div class="btn-group listFooter" role="group" aria-label="" id="b-group">
-					<button type="button" class="btn btn-info">수정하기</button>
-					<button type="button" class="btn btn-warning">판매중지</button>
-					<button type="button" class="btn btn-danger">삭제하기</button>
-				</div>
-				<!-- 섬네일 끝 -->
-
-				<!-- 여기서부터 섬네일 시작 -->
-				<div class="contents_list1">
-					<div class="list_image1">
-						<div class="statusBand transblack">
-							<p id="status" name="status">2015.1.12 판매시작<br>
-							<span class="sub_title"><span id="pNumber">124</span>명의 고객이 다녀갔습니다.</span>
-							</p>
-						</div>
-						<div class="band">
-							<div class="wishcount">
-								<img src="/application/views/images/main/img23_heart.PNG" alt="" class="heart_image">
-								<span id="heart" name="heart"> 15</span> 
-							</div>
-							<div class="reviewcount">
-								<img src="/application/views/images/main/img24_talkbox.PNG" alt="" class="talkbox_image">
-								<span id="talkbox" name="talkbox"> 11</span>
-							</div>
-							<div class="localcount">
-								<img src="/application/views/images/main/img27_navi.PNG" alt="" class="navi_image">
-								<span id="talkbox" name="talkbox"> 当地人</span>
-							</div>
-						</div>
-						<img src="/application/views/images/contents/img01.jpg" alt="" class="pic1_img" style="width: 100%; height: 100%;">
-						<img src="/application/views/images/main/profile01.jpg" alt="" class="img-circle profile_image">
-						<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
-						<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
-					</div><!-- list_image1 닫힘 -->
-					<div class="list_rightwrap whitegloss">
-						<div class="right_title"><span>纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题</span>
-						</div>
-						<div class="money greybluegloss"><span>500元</span></div>
-						<div class="right_text">纽约是什么样的呢？是电影中午夜时分的灯红酒绿，还是黑暗阴影中的危机四伏？每个人心目中都有一副纽约的图像，诚然，纽约是一座集经济、科技、旅游、娱乐、影视、时尚于一身的豪华大城市。这里是旅游之都，高耸的帝国大厦，散发着耀眼的光芒， 就如同霸气一身的王者；被誉为“照耀世界之神”的自由女神像，彰显着美丽的容颜和非凡的气质，令无数的游客折服，每一个到美国的人都不会错过。这里也是购物之都，人潮拥挤的第五大道，是购物狂的天堂，你几乎可以买到世界上任何的名牌。这里还是经济之都，在气氛紧张的纽约证劵交易所里，每天都上演着经济大战......不要只停留在美剧当中的纽约啦，赶快出发吧，玩转纽约，来一场说走就走的旅行吧！ 
-						</div>
-						<div class="bottom_text">
-							<div class="line1">
-								<div class="tag"><img src="/application/views/images/left_menu/icon11_adventure.PNG" alt="">冒险</div>
-								<div class="tag"><img src="/application/views/images/left_menu/icon12_relaxation.PNG" alt="">休闲</div>
-								<div class="tag"><img src="/application/views/images/left_menu/icon13_romantic.PNG" alt="">浪漫</div>
-							</div>
-							<div class="line2">
-								<div class="tag2"><img src="/application/views/images/left_menu/icon21_solo.PNG" alt="">单人</div>
-								<div class="tag2"><img src="/application/views/images/left_menu/icon23_family.PNG" alt="">家族</div>
-							</div>
-						</div> <!-- bottom_text 닫음 -->
-					</div> <!-- list_rightwrap 닫음 -->
-				</div><!-- contents_list1 닫음 -->
-				<!-- 섬네일 끝 -->
-
-				<!-- 여기서부터 섬네일 시작 -->
-				<div class="contents_list1">
-					<div class="list_image1">
-						<div class="statusBand transblack">
-							<p id="status" name="status">2015.1.12 판매시작<br>
-							<span class="sub_title"><span id="pNumber">150</span>명의 고객이 다녀갔습니다.</span>
-							</p>
-						</div>
-						<div class="band">
-							<div class="wishcount">
-								<img src="/application/views/images/main/img23_heart.PNG" alt="" class="heart_image">
-								<span id="heart" name="heart"> 15</span> 
-							</div>
-							<div class="reviewcount">
-								<img src="/application/views/images/main/img24_talkbox.PNG" alt="" class="talkbox_image">
-								<span id="talkbox" name="talkbox"> 11</span>
-							</div>
-							<div class="localcount">
-								<img src="/application/views/images/main/img27_navi.PNG" alt="" class="navi_image">
-								<span id="talkbox" name="talkbox"> 当地人</span>
-							</div>
-						</div>
-						<img src="/application/views/images/contents/img01.jpg" alt="" class="pic1_img" style="width: 100%; height: 100%;">
-						<img src="/application/views/images/main/profile01.jpg" alt="" class="img-circle profile_image">
-						<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
-						<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
-					</div><!-- list_image1 닫힘 -->
-					<div class="list_rightwrap whitegloss">
-						<div class="right_title"><span>纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题</span>
-						</div>
-						<div class="money greybluegloss"><span>500元</span></div>
-						<div class="right_text">纽约是什么样的呢？是电影中午夜时分的灯红酒绿，还是黑暗阴影中的危机四伏？每个人心目中都有一副纽约的图像，诚然，纽约是一座集经济、科技、旅游、娱乐、影视、时尚于一身的豪华大城市。这里是旅游之都，高耸的帝国大厦，散发着耀眼的光芒， 就如同霸气一身的王者；被誉为“照耀世界之神”的自由女神像，彰显着美丽的容颜和非凡的气质，令无数的游客折服，每一个到美国的人都不会错过。这里也是购物之都，人潮拥挤的第五大道，是购物狂的天堂，你几乎可以买到世界上任何的名牌。这里还是经济之都，在气氛紧张的纽约证劵交易所里，每天都上演着经济大战......不要只停留在美剧当中的纽约啦，赶快出发吧，玩转纽约，来一场说走就走的旅行吧！ 
-						</div>
-						<div class="bottom_text">
-							<div class="line1">
-								<div class="tag"><img src="/application/views/images/left_menu/icon11_adventure.PNG" alt="">冒险</div>
-								<div class="tag"><img src="/application/views/images/left_menu/icon12_relaxation.PNG" alt="">休闲</div>
-								<div class="tag"><img src="/application/views/images/left_menu/icon13_romantic.PNG" alt="">浪漫</div>
-							</div>
-							<div class="line2">
-								<div class="tag2"><img src="/application/views/images/left_menu/icon21_solo.PNG" alt="">单人</div>
-								<div class="tag2"><img src="/application/views/images/left_menu/icon23_family.PNG" alt="">家族</div>
-							</div>
-						</div> <!-- bottom_text 닫음 -->
-					</div> <!-- list_rightwrap 닫음 -->
-				</div><!-- contents_list1 닫음 -->
-				<!-- 섬네일 끝 -->
-				
 			</div><!-- pmWrap 끝 -->
 		</div><!-- profileWrap1 -->
---
+++ application/views/mypage/m_guide2_tab1.php
@@ -0,0 +1,168 @@
+				<!-- 여기서부터 탭 영역 시작 Page -->
+				<div id="tabplace">
+				<div class="btn-group sortGroup" role="group" aria-label="" id="sortGroup">
+					<button type="button" class="btn btn-info" id="sort1" name="sort1">등록순</button>
+					<button type="button" class="btn btn-info" id="sort2" name="sort2">후기순</button>
+					<button type="button" class="btn btn-info" id="sort3" name="sort3">판매순</button>
+					<button type="button" class="btn btn-info" id="sort4" name="sort4">좋아요순</button>
+				</div>
+
+				<!-- 여기서부터 섬네일 시작 -->
+				<div class="contents_list1">
+					<div class="list_image1">
+						<div class="statusBand transblack">
+							<p id="status" name="status">2015.1.12 판매시작<br>
+							<span class="sub_title"><span id="pNumber">160</span>명의 고객이 다녀갔습니다.</span>
+							</p>
+						</div>
+						<div class="band">
+							<div class="wishcount">
+								<img src="/application/views/images/main/img23_heart.PNG" alt="" class="heart_image">
+								<span id="heart" name="heart"> 15</span> 
+							</div>
+							<div class="reviewcount">
+								<img src="/application/views/images/main/img24_talkbox.PNG" alt="" class="talkbox_image">
+								<span id="talkbox" name="talkbox"> 11</span>
+							</div>
+							<div class="localcount">
+								<img src="/application/views/images/main/img27_navi.PNG" alt="" class="navi_image">
+								<span id="talkbox" name="talkbox"> 当地人</span>
+							</div>
+						</div>
+						<img src="/application/views/images/contents/img01.jpg" alt="" class="pic1_img" style="width: 100%; height: 100%;">
+						<img src="/application/views/images/main/profile01.jpg" alt="" class="img-circle profile_image">
+						<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
+						<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
+					</div><!-- list_image1 닫힘 -->
+					<div class="list_rightwrap whitegloss">
+						<div class="right_title"><span>纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题</span>
+						</div>
+						<div class="money bluegloss"><span>500元</span></div>
+						<div class="right_text">纽约是什么样的呢？是电影中午夜时分的灯红酒绿，还是黑暗阴影中的危机四伏？每个人心目中都有一副纽约的图像，诚然，纽约是一座集经济、科技、旅游、娱乐、影视、时尚于一身的豪华大城市。这里是旅游之都，高耸的帝国大厦，散发着耀眼的光芒， 就如同霸气一身的王者；被誉为“照耀世界之神”的自由女神像，彰显着美丽的容颜和非凡的气质，令无数的游客折服，每一个到美国的人都不会错过。这里也是购物之都，人潮拥挤的第五大道，是购物狂的天堂，你几乎可以买到世界上任何的名牌。这里还是经济之都，在气氛紧张的纽约证劵交易所里，每天都上演着经济大战......不要只停留在美剧当中的纽约啦，赶快出发吧，玩转纽约，来一场说走就走的旅行吧！ 
+						</div>
+						<div class="bottom_text">
+							<div class="line1">
+								<div class="tag"><img src="/application/views/images/left_menu/icon11_adventure.PNG" alt="">冒险</div>
+								<div class="tag"><img src="/application/views/images/left_menu/icon12_relaxation.PNG" alt="">休闲</div>
+								<div class="tag"><img src="/application/views/images/left_menu/icon13_romantic.PNG" alt="">浪漫</div>
+							</div>
+							<div class="line2">
+								<div class="tag2"><img src="/application/views/images/left_menu/icon21_solo.PNG" alt="">单人</div>
+								<div class="tag2"><img src="/application/views/images/left_menu/icon23_family.PNG" alt="">家族</div>
+							</div>
+						</div> <!-- bottom_text 닫음 -->
+					</div> <!-- list_rightwrap 닫음 -->
+				</div><!-- contents_list1 닫음 -->
+				<div class="btn-group listFooter" role="group" aria-label="" id="b-group">
+					<button type="button" class="btn whitegloss bd" id="productModify" name="productModify">Modify</button>
+					<button type="button" class="btn whitegloss bd" id="productStop" name="productStop">Stop</button>
+					<button type="button" class="btn whitegloss bd" id="productDelete" name="productDelete">Delete</button>
+				</div>
+				<!-- 섬네일 끝 -->
+
+				<!-- 여기서부터 섬네일 시작 -->
+				<div class="contents_list1">
+					<div class="list_image1">
+						<div class="statusBand transblack">
+							<p id="status" name="status">2015.1.12 판매시작<br>
+							<span class="sub_title"><span id="pNumber">124</span>명의 고객이 다녀갔습니다.</span>
+							</p>
+						</div>
+						<div class="band">
+							<div class="wishcount">
+								<img src="/application/views/images/main/img23_heart.PNG" alt="" class="heart_image">
+								<span id="heart" name="heart"> 15</span> 
+							</div>
+							<div class="reviewcount">
+								<img src="/application/views/images/main/img24_talkbox.PNG" alt="" class="talkbox_image">
+								<span id="talkbox" name="talkbox"> 11</span>
+							</div>
+							<div class="localcount">
+								<img src="/application/views/images/main/img27_navi.PNG" alt="" class="navi_image">
+								<span id="talkbox" name="talkbox"> 当地人</span>
+							</div>
+						</div>
+						<img src="/application/views/images/contents/img02.jpg" alt="" class="pic1_img" style="width: 100%; height: 100%;">
+						<img src="/application/views/images/main/profile02.jpg" alt="" class="img-circle profile_image">
+						<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
+						<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
+					</div><!-- list_image1 닫힘 -->
+					<div class="list_rightwrap whitegloss">
+						<div class="right_title"><span>纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题</span>
+						</div>
+						<div class="money bluegloss"><span>500元</span></div>
+						<div class="right_text">纽约是什么样的呢？是电影中午夜时分的灯红酒绿，还是黑暗阴影中的危机四伏？每个人心目中都有一副纽约的图像，诚然，纽约是一座集经济、科技、旅游、娱乐、影视、时尚于一身的豪华大城市。这里是旅游之都，高耸的帝国大厦，散发着耀眼的光芒， 就如同霸气一身的王者；被誉为“照耀世界之神”的自由女神像，彰显着美丽的容颜和非凡的气质，令无数的游客折服，每一个到美国的人都不会错过。这里也是购物之都，人潮拥挤的第五大道，是购物狂的天堂，你几乎可以买到世界上任何的名牌。这里还是经济之都，在气氛紧张的纽约证劵交易所里，每天都上演着经济大战......不要只停留在美剧当中的纽约啦，赶快出发吧，玩转纽约，来一场说走就走的旅行吧！ 
+						</div>
+						<div class="bottom_text">
+							<div class="line1">
+								<div class="tag"><img src="/application/views/images/left_menu/icon11_adventure.PNG" alt="">冒险</div>
+								<div class="tag"><img src="/application/views/images/left_menu/icon12_relaxation.PNG" alt="">休闲</div>
+								<div class="tag"><img src="/application/views/images/left_menu/icon13_romantic.PNG" alt="">浪漫</div>
+							</div>
+							<div class="line2">
+								<div class="tag2"><img src="/application/views/images/left_menu/icon21_solo.PNG" alt="">单人</div>
+								<div class="tag2"><img src="/application/views/images/left_menu/icon23_family.PNG" alt="">家族</div>
+							</div>
+						</div> <!-- bottom_text 닫음 -->
+					</div> <!-- list_rightwrap 닫음 -->
+				</div><!-- contents_list1 닫음 -->
+				<div class="btn-group listFooter" role="group" aria-label="" id="b-group">
+					<button type="button" class="btn whitegloss bd" id="productModify" name="productModify">Modify</button>
+					<button type="button" class="btn whitegloss bd" id="productStop" name="productStop">Stop</button>
+					<button type="button" class="btn whitegloss bd" id="productDelete" name="productDelete">Delete</button>
+				</div>
+				<!-- 섬네일 끝 -->
+
+				<!-- 여기서부터 섬네일 시작 -->
+				<div class="contents_list1">
+					<div class="list_image1">
+						<div class="statusBand transblack">
+							<p id="status" name="status">2015.1.12 판매시작<br>
+							<span class="sub_title"><span id="pNumber">150</span>명의 고객이 다녀갔습니다.</span>
+							</p>
+						</div>
+						<div class="band">
+							<div class="wishcount">
+								<img src="/application/views/images/main/img23_heart.PNG" alt="" class="heart_image">
+								<span id="heart" name="heart"> 15</span> 
+							</div>
+							<div class="reviewcount">
+								<img src="/application/views/images/main/img24_talkbox.PNG" alt="" class="talkbox_image">
+								<span id="talkbox" name="talkbox"> 11</span>
+							</div>
+							<div class="localcount">
+								<img src="/application/views/images/main/img27_navi.PNG" alt="" class="navi_image">
+								<span id="talkbox" name="talkbox"> 当地人</span>
+							</div>
+						</div>
+						<img src="/application/views/images/contents/img03.jpg" alt="" class="pic1_img" style="width: 100%; height: 100%;">
+						<img src="/application/views/images/main/profile03.jpg" alt="" class="img-circle profile_image">
+						<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
+						<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
+					</div><!-- list_image1 닫힘 -->
+					<div class="list_rightwrap whitegloss">
+						<div class="right_title"><span>纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题纽约军事历史主题</span>
+						</div>
+						<div class="money bluegloss"><span>500元</span></div>
+						<div class="right_text">纽约是什么样的呢？是电影中午夜时分的灯红酒绿，还是黑暗阴影中的危机四伏？每个人心目中都有一副纽约的图像，诚然，纽约是一座集经济、科技、旅游、娱乐、影视、时尚于一身的豪华大城市。这里是旅游之都，高耸的帝国大厦，散发着耀眼的光芒， 就如同霸气一身的王者；被誉为“照耀世界之神”的自由女神像，彰显着美丽的容颜和非凡的气质，令无数的游客折服，每一个到美国的人都不会错过。这里也是购物之都，人潮拥挤的第五大道，是购物狂的天堂，你几乎可以买到世界上任何的名牌。这里还是经济之都，在气氛紧张的纽约证劵交易所里，每天都上演着经济大战......不要只停留在美剧当中的纽约啦，赶快出发吧，玩转纽约，来一场说走就走的旅行吧！ 
+						</div>
+						<div class="bottom_text">
+							<div class="line1">
+								<div class="tag"><img src="/application/views/images/left_menu/icon11_adventure.PNG" alt="">冒险</div>
+								<div class="tag"><img src="/application/views/images/left_menu/icon12_relaxation.PNG" alt="">休闲</div>
+								<div class="tag"><img src="/application/views/images/left_menu/icon13_romantic.PNG" alt="">浪漫</div>
+							</div>
+							<div class="line2">
+								<div class="tag2"><img src="/application/views/images/left_menu/icon21_solo.PNG" alt="">单人</div>
+								<div class="tag2"><img src="/application/views/images/left_menu/icon23_family.PNG" alt="">家族</div>
+							</div>
+						</div> <!-- bottom_text 닫음 -->
+					</div> <!-- list_rightwrap 닫음 -->
+				</div><!-- contents_list1 닫음 -->
+				<div class="btn-group listFooter" role="group" aria-label="" id="b-group">
+					<button type="button" class="btn whitegloss bd" id="productModify" name="productModify">Modify</button>
+					<button type="button" class="btn whitegloss bd" id="productStop" name="productStop">Stop</button>
+					<button type="button" class="btn whitegloss bd" id="productDelete" name="productDelete">Delete</button>
+				</div>
+				<!-- 섬네일 끝 -->
+			</div><!-- 탭영역 끝 -->
