ccee7647be8cb4874a7915e9e1f4f4602711048d
sigurae723 <sbs@linkasia.co.kr>
2015-01-13 15:58:26 +0900

    2015.01.13
    Chatlist  작업 전 Sync
    Guide페이지 작업 동기화

---
+++ application/views/contents/chating.php
@@ -0,0 +1,143 @@
+<div id="chatlistWrap">
+
+	<div class="header">
+		<img src="/application/views/images/flag/20american.png" alt="nation_flagImage" class="flag">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<button class="btn btn-start">Newyork</button>
+		<p class="chatingTitle">《老友记》咖啡馆纽约开张免费迎客</p>
+	</div><!-- div.header end -->
+
+	<div class="chatingFrame">	
+		<div id="youChat">
+			<span id="youchatID">Hye-ri <span id="youchatDate">2015年10月20日 18:10</span></span>
+			<div id="chatImg" class="chatImg">
+				<img src="/application/views/images/main/profile01.jpg" alt="" class="profile_image img-circle">
+				<img src="/application/views/images/flag/20american.png" alt="nation_flagImage" class="flag_image">
+				<img src="/application/views/images/main/img22_vmark.png" alt="" class="vmark_image">
+			</div>
+			<img src="/application/views/images/contents/chatarrow_left.png" alt="" class="chatarrow1">
+			<div class="youchatBox"><p>距此次《查理周刊》袭击事件不久前，法国曾连续3日发生袭击事件。虽然法当局并未确认这与恐袭有关，并为一连串袭击降温，但有媒体称，这些事件是“疑似恐怖攻击的‘独狼式袭击’”。</p></div>
+		</div><!-- youChat end -->
+
+		<div id="meChat">
+			<span id="mechatID">Hyo-Sung <span id="mechatDate">2015年10月20日 18:12</span></span>
+			<div id="chatImg" class="chatImg">
+				<img src="/application/views/images/main/profile02.jpg" alt="" class="profile_image img-circle">
+				<img src="/application/views/images/flag/20american.png" alt="nation_flagImage" class="flag_image">
+				<img src="/application/views/images/main/img22_vmark.png" alt="" class="vmark_image">
+			</div>
+			<img src="/application/views/images/contents/chatarrow_right.png" alt="" class="chatarrow2">
+			<div class="mechatBox"><p>1月5日，在位于达州市达川区麻柳镇明月江的一条支流的河床上打捞起一根长22米、重超60吨、价值不菲的千年乌木。据在现场的专家介绍，该段乌木被深埋于数10米深的河床已有3000余年，</p></div>
+		</div>
+
+		<div id="youChat">
+			<span id="youchatID">Hye-ri <span id="youchatDate">2015年10月20日 18:15</span></span>
+			<div id="chatImg" class="chatImg">
+				<img src="/application/views/images/main/profile01.jpg" alt="" class="profile_image img-circle">
+				<img src="/application/views/images/flag/20american.png" alt="nation_flagImage" class="flag_image">
+				<img src="/application/views/images/main/img22_vmark.png" alt="" class="vmark_image">
+			</div>
+			<img src="/application/views/images/contents/chatarrow_left.png" alt="" class="chatarrow1">
+			<div class="youchatBox"><p>且保存完好，木质坚硬。乌木购买主于1月5日和6日组织两辆挖掘机，在河滩上昼夜不停地施工两个昼夜，终于将该段乌木刨出水面，抬至河岸边。然而，当一切准备妥当后，该段体型巨大的乌木因无法装上大卡车运走外地。</p></div>
+		</div><!-- youChat end -->
+
+		<div id="meChat">
+			<span id="mechatID">Hyo-Sung <span id="mechatDate">2015年10月20日 18:20</span></span>
+			<div id="chatImg" class="chatImg">
+				<img src="/application/views/images/main/profile02.jpg" alt="" class="profile_image img-circle">
+				<img src="/application/views/images/flag/20american.png" alt="nation_flagImage" class="flag_image">
+				<img src="/application/views/images/main/img22_vmark.png" alt="" class="vmark_image">
+			</div>
+			<img src="/application/views/images/contents/chatarrow_right.png" alt="" class="chatarrow2">
+			<div class="mechatBox"><p>经买主合议之后，命令施工人员将乌木锯成了17米和5米长的两截，并分批装载上车后驶离施工现场。</p></div>
+		</div>
+
+		<div id="youChat">
+			<span id="youchatID">Hye-ri <span id="youchatDate">2015年10月20日 18:45</span></span>
+			<div id="chatImg" class="chatImg">
+				<img src="/application/views/images/main/profile01.jpg" alt="" class="profile_image img-circle">
+				<img src="/application/views/images/flag/20american.png" alt="nation_flagImage" class="flag_image">
+				<img src="/application/views/images/main/img22_vmark.png" alt="" class="vmark_image">
+			</div>
+			<img src="/application/views/images/contents/chatarrow_left.png" alt="" class="chatarrow1">
+			<div class="youchatBox"><p>1月5日-6日两天，在达川区麻柳镇的河滩上聚集不少人，围观当地被埋在河床下的宝贝——乌木。随着两辆大型挖掘机挺进达川区麻柳镇的明月江支流，在河滩上不停施工挖掘，一根体型巨大，深埋地底的乌木逐渐清晰。
+
+负责现场治安工作的达川区麻柳镇民警介绍，乌木被深埋在河床10余米的淤泥中，且树木表皮保存完好，木质坚硬，其经济价值巨大。为成功打捞起该段乌木，当天施工部门不仅租用两辆大型挖掘机，还雇佣了数台运输装载的车辆与机器。</p></div>
+		</div><!-- youChat end -->
+	</div><!-- chatingFrame end -->
+
+
+	<div class="rightWrap">
+		<div class="rightProfile">
+			<p class="guideTalk"><span class="glyphicon glyphicon-comment"></span> Guide Talk</p>
+			<div class="gray_box"><span><img src="/application/views/images/main/img29_navi.PNG" alt="">  当地人</span>
+				<img src="/application/views/images/main/profile01.jpg" alt="" class="img-circle profile_image">
+				<img src="/application/views/images/flag/01korea.png" alt="" class="flag_image">
+				<img src="/application/views/images/main/img22_vmark.PNG" alt="" class="vmark_image">
+			</div>
+			<div class="name_tag">
+				<div class="tag1">Advanced</div>
+				<div class="tag2">Funny</div>
+				<div class="tag3">History</div>
+			</div>
+			<div class="text">
+				<p>
+					生活在 : Seoul <br>
+					现居住在纽约 : 2015年 <br>
+					性别 : 女<br>
+					语言 : Korean, English, Chinese<br>
+				</p>
+			</div>
+			<div class="sns_icon">
+				<!-- <img src="/application/views/images/main/sns03G.png" alt="" class="sns_icon1G"> -->
+				<img src="/application/views/images/main/sns03.png" alt="" class="sns_icon1C">
+				<img src="/application/views/images/main/sns02G.png" alt="" class="sns_icon2C">
+				<!-- <img src="/application/views/images/main/sns02.png" alt="" class="sns_icon2G"> -->
+			</div><!-- sns_icon end -->
+		</div><!-- rightProfile end -->
+
+		<div class="bookedDay">
+			<div class="band">BookedDay</div>
+
+			<div id="reservationDay">
+				<span id="bookedDate">2015/01/08</span>
+				<input type="text" class="form-control" placeholder="" id="number">
+				<span>名   </span><span class="glyphicon glyphicon-remove" id="remove"></span>
+			</div><!-- reservationDay 이게 반복되면서 늘어남 -->
+			<div id="reservationDay">
+				<span id="bookedDate">2015/01/08</span>
+				<input type="text" class="form-control" placeholder="" id="number">
+				<span>名   </span><span class="glyphicon glyphicon-remove" id="remove"></span>
+			</div><!-- reservationDay 이게 반복되면서 늘어남 -->
+			<div id="reservationDay">
+				<span id="bookedDate">2015/01/08</span>
+				<input type="text" class="form-control" placeholder="" id="number">
+				<span>名   </span><span class="glyphicon glyphicon-remove" id="remove"></span>
+			</div><!-- reservationDay 이게 반복되면서 늘어남 -->
+			<div id="reservationDay">
+				<span id="bookedDate">2015/01/08</span>
+				<input type="text" class="form-control" placeholder="" id="number">
+				<span>名   </span><span class="glyphicon glyphicon-remove" id="remove"></span>
+			</div><!-- reservationDay 이게 반복되면서 늘어남 -->
+
+		</div>
+
+		<div class="Add">
+			<span id="fixedPrice">Fixed price From Guide</span>
+			<div class="statusDiv" id="statusDiv"><p>판매가격 입력 대기중...</p></div>
+		</div>
+
+		<div class="Add">
+			<span id="fixedPrice">Fixed price From Guide</span>
+			<input type="text" class="form-control" id="inputPrice"><span class="won">元</span>
+			<button class="btn btn-info" id="priceModify">가격수정</button>
+			<button class="btn btn-warning" id="pricePay">결제하기</button>
+		</div>
+	</div><!-- rightWrap end -->
+
+	<div id="typingBox">
+		<input type="text" name="" id="chatInput" class="form-control" cols="30" rows="10" placeholder="Write a message..."></input>
+		<span class="btn btn-primary" id="chatSend">Send</span>
+	</div>
+</div><!-- chatlistWrap -->
+
--- application/views/mypage/이전파일 - 아직지우지마세요/z.php
+++ application/views/mypage/이전파일 - 아직지우지마세요/z.php
@@ -1,42 +1,22 @@
-		<div class="profileWrap1">
-			<div id="serviceStatus" class="bd whitegloss">
-				<p class="title bluegloss2">서비스 제공 예정일</p>
-				<div class="serviceWindow bd white">
-					<p id="status1">3일뒤 ~2015.1.16 : <span class="txt_blue" id="status1ID">james</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">4일뒤 ~2015.1.17 : <span class="txt_blue" id="status1ID">hhc</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">5일뒤 ~2015.1.18 : <span class="txt_blue" id="status1ID">sbs</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">6일뒤 ~2015.1.19 : <span class="txt_blue" id="status1ID">abc</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">7일뒤 ~2015.1.20 : <span class="txt_blue" id="status1ID">def</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">3일뒤 ~2015.1.16 : <span class="txt_blue" id="status1ID">james</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">4일뒤 ~2015.1.17 : <span class="txt_blue" id="status1ID">hhc</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">5일뒤 ~2015.1.18 : <span class="txt_blue" id="status1ID">sbs</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">6일뒤 ~2015.1.19 : <span class="txt_blue" id="status1ID">abc</span>에게 가이드 제공하기로 예약됐습니다.</p>
-					<p id="status1">7일뒤 ~2015.1.20 : <span class="txt_blue" id="status1ID">def</span>에게 가이드 제공하기로 예약됐습니다.</p>
-				</div>
-			</div><!-- serviceStatus end -->
-
-			<div class="title1">
-				<div class="underline">판매관리</div>
-			</div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
-			<div class="btn-group sortGroup" role="group" aria-label="" id="salessortGroup">
-				<button type="button" class="btn btn-info" id="sort1" name="sort1">전체보기<span id="sortNumber1">(42)</span></button>
-				<button type="button" class="btn btn-info" id="sort2" name="sort2">구매결정<span id="sortNumber2">(4)</span></button>
-				<button type="button" class="btn btn-info" id="sort3" name="sort3">가격결정<span id="sortNumber3">(20)</span></button>
-				<button type="button" class="btn btn-info" id="sort4" name="sort4">결제완료<span id="sortNumber4">(25)</span></button>
-				<button type="button" class="btn btn-info" id="sort4" name="sort4">입금완료<span id="sortNumber5">(10)</span></button>
+		<div class="hostWrap">
+			<div class="title2">
+				<div class="underline">当地人 - 商品</div>
+			</div>
+			<div class="sort_button">
+				<button class="btn btn-info button1" id="button1">全部(42)</button>
+				<button class="btn btn-info button2" id="button2">咨询中(5)</button>
+				<button class="btn btn-info button3" id="button3">按游记(3)</button>
+				<button class="btn btn-info button4" id="button4">按时间(8)</button>
 			</div>
 
-
-			<!---------------- 상품 반복시작 ------------------->
 			<div class="salesList">
-				<input type="checkbox" id="salesCheckbox" name="salesCheckbox">
 				<div class="titlediv">
-					<span class="salestitle" id="salestitle" name="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆最为惊喜的各大博物馆最为惊喜的各大博物馆</span>
+					<span class="salestitle" id="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆</span>
 				</div><!-- titlediv 닫음 -->
-				<div class="banddiv whitegloss">
-					<span class="bandtitle" id="bandtitle" name="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2" name="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
+				<div class="banddiv">
+					<span class="bandtitle" id="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
 					<img src="/application/views/images/main/profile01.jpg" alt="profile_image" class="img-circle profile_image">
-					<img src="/application/views/images/flag/01korea.png" alt="flag_image" class="flag_image">
+					<img src="/application/views/images/left_menu/flag03_usa.png" alt="flag_image" class="flag_image">
 					<img src="/application/views/images/main/img22_vmark.PNG" alt="vmark_image" class="vmark_image">
 				</div><!-- banddiv 닫음 -->
 				<div class="stepdiv">
@@ -74,16 +54,14 @@
 				</div><!-- stepdiv 닫음 -->
 			</div><!-- salesList 닫음, 이 박스가 반복됨-->
 
-			<!-- 상품 반복시작 -->
 			<div class="salesList">
-				<input type="checkbox" id="salesCheckbox" name="salesCheckbox">
 				<div class="titlediv">
-					<span class="salestitle" id="salestitle" name="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆最为惊喜的各大博物馆最为惊喜的各大博物馆</span>
+					<span class="salestitle" id="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，</span>
 				</div><!-- titlediv 닫음 -->
-				<div class="banddiv whitegloss">
-					<span class="bandtitle" id="bandtitle" name="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2" name="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
-					<img src="/application/views/images/main/profile01.jpg" alt="profile_image" class="img-circle profile_image">
-					<img src="/application/views/images/flag/01korea.png" alt="flag_image" class="flag_image">
+				<div class="banddiv">
+					<span class="bandtitle" id="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
+					<img src="/application/views/images/main/profile02.jpg" alt="profile_image" class="img-circle profile_image">
+					<img src="/application/views/images/left_menu/flag03_usa.png" alt="flag_image" class="flag_image">
 					<img src="/application/views/images/main/img22_vmark.PNG" alt="vmark_image" class="vmark_image">
 				</div><!-- banddiv 닫음 -->
 				<div class="stepdiv">
@@ -98,12 +76,12 @@
 						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow02" id="icon_arrow02">
 						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow02g" id="icon_arrow02g"> -->
 
-						<img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03" id="icon_sale03">
+						<img src="/application/views/images/mypage/icon_sale03.png" alt="sale_icon_image" class="icon_sale03" id="icon_sale03">
 						<!-- <img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03g" id="icon_sale03g"> -->
-						<img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03" id="icon_arrow03g">
+						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow03" id="icon_arrow03">
 						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03g" id="icon_arrow03g"> -->
 
-						<img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04" id="icon_sale04">
+						<img src="/application/views/images/mypage/icon_sale04.png" alt="sale_icon_image" class="icon_sale04" id="icon_sale04">
 						<!-- <img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04g" id="icon_sale04g"> -->
 					</div><!-- stepImage 닫음 -->
 					<div class="stepText">
@@ -121,16 +99,14 @@
 				</div><!-- stepdiv 닫음 -->
 			</div><!-- salesList 닫음, 이 박스가 반복됨-->
 
-			<!-- 상품 반복시작 -->
 			<div class="salesList">
-				<input type="checkbox" id="salesCheckbox" name="salesCheckbox">
 				<div class="titlediv">
-					<span class="salestitle" id="salestitle" name="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆最为惊喜的各大博物馆最为惊喜的各大博物馆</span>
+					<span class="salestitle" id="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆</span>
 				</div><!-- titlediv 닫음 -->
-				<div class="banddiv whitegloss">
-					<span class="bandtitle" id="bandtitle" name="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2" name="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
-					<img src="/application/views/images/main/profile01.jpg" alt="profile_image" class="img-circle profile_image">
-					<img src="/application/views/images/flag/01korea.png" alt="flag_image" class="flag_image">
+				<div class="banddiv">
+					<span class="bandtitle" id="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
+					<img src="/application/views/images/main/profile03.jpg" alt="profile_image" class="img-circle profile_image">
+					<img src="/application/views/images/left_menu/flag03_usa.png" alt="flag_image" class="flag_image">
 					<img src="/application/views/images/main/img22_vmark.PNG" alt="vmark_image" class="vmark_image">
 				</div><!-- banddiv 닫음 -->
 				<div class="stepdiv">
@@ -145,12 +121,57 @@
 						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow02" id="icon_arrow02">
 						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow02g" id="icon_arrow02g"> -->
 
-						<img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03" id="icon_sale03">
+						<img src="/application/views/images/mypage/icon_sale03.png" alt="sale_icon_image" class="icon_sale03" id="icon_sale03">
 						<!-- <img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03g" id="icon_sale03g"> -->
-						<img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03" id="icon_arrow03g">
+						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow03" id="icon_arrow03">
 						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03g" id="icon_arrow03g"> -->
 
-						<img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04" id="icon_sale04">
+						<img src="/application/views/images/mypage/icon_sale04.png" alt="sale_icon_image" class="icon_sale04" id="icon_sale04">
+						<!-- <img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04g" id="icon_sale04g"> -->
+					</div><!-- stepImage 닫음 -->
+					<div class="stepText">
+						<span class="step1">咨询中</span>
+						<span class="step2">预定成功</span>
+						<span class="step3">服务结束</span>
+						<span class="step4">已付款</span>
+					</div>
+
+					<div class="absoluteDiv">
+						<span>价格</span><br>
+						<span class="font_blue price">1500元</span><br>
+						<span class="btn btn-info">取消交易</span>
+					</div>
+				</div><!-- stepdiv 닫음 -->
+			</div><!-- salesList 닫음, 이 박스가 반복됨-->
+
+			<div class="salesList">
+				<div class="titlediv">
+					<span class="salestitle" id="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆</span>
+				</div><!-- titlediv 닫음 -->
+				<div class="banddiv">
+					<span class="bandtitle" id="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
+					<img src="/application/views/images/main/profile04.jpg" alt="profile_image" class="img-circle profile_image">
+					<img src="/application/views/images/left_menu/flag03_usa.png" alt="flag_image" class="flag_image">
+					<img src="/application/views/images/main/img22_vmark.PNG" alt="vmark_image" class="vmark_image">
+				</div><!-- banddiv 닫음 -->
+				<div class="stepdiv">
+					<div class="stepImage">
+						<img src="/application/views/images/mypage/icon_sale01.png" alt="sale_icon_image" class="icon_sale01" id="icon_sale01">
+						<!-- <img src="/application/views/images/mypage/icon_sale01g.png" alt="sale_icon_image" class="icon_sale01g" id="icon_sale01g"> -->
+						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow01" id="icon_arrow01">
+						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow01g" id="icon_arrow01g"> -->
+
+						<img src="/application/views/images/mypage/icon_sale02.png" alt="sale_icon_image" class="icon_sale02" id="icon_sale02">
+						<!-- <img src="/application/views/images/mypage/icon_sale02g.png" alt="sale_icon_image" class="icon_sale02g" id="icon_sale02g"> -->
+						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow02" id="icon_arrow02">
+						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow02g" id="icon_arrow02g"> -->
+
+						<img src="/application/views/images/mypage/icon_sale03.png" alt="sale_icon_image" class="icon_sale03" id="icon_sale03">
+						<!-- <img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03g" id="icon_sale03g"> -->
+						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow03" id="icon_arrow03">
+						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03g" id="icon_arrow03g"> -->
+
+						<img src="/application/views/images/mypage/icon_sale04.png" alt="sale_icon_image" class="icon_sale04" id="icon_sale04">
 						<!-- <img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04g" id="icon_sale04g"> -->
 					</div><!-- stepImage 닫음 -->
 					<div class="stepText">
@@ -166,70 +187,6 @@
 						<span class="btn btn-info">取消交易</span>
 					</div>
 				</div><!-- stepdiv 닫음 -->
-			</div><!-- salesList 닫음, 이 박스가 반복됨-->
+			</div><!-- salesList 닫음, 이 박스가 반복됨-->		
 
-			<!-- 상품 반복시작 -->
-			<div class="salesList">
-				<input type="checkbox" id="salesCheckbox" name="salesCheckbox">
-				<div class="titlediv">
-					<span class="salestitle" id="salestitle" name="salestitle">一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆，一座古典与时尚融合的非常好的城市。最为惊喜的各大博物馆最为惊喜的各大博物馆最为惊喜的各大博物馆</span>
-				</div><!-- titlediv 닫음 -->
-				<div class="banddiv whitegloss">
-					<span class="bandtitle" id="bandtitle" name="bandtitle">最近联系 : 2014. 6. 9</span><span class="bandtitle2" id="bandtitle2" name="bandtitle2">预约日期 : 2014. 12. 15,  2014. 12. 16,  2014. 12. 17</span>
-					<img src="/application/views/images/main/profile01.jpg" alt="profile_image" class="img-circle profile_image">
-					<img src="/application/views/images/flag/01korea.png" alt="flag_image" class="flag_image">
-					<img src="/application/views/images/main/img22_vmark.PNG" alt="vmark_image" class="vmark_image">
-				</div><!-- banddiv 닫음 -->
-				<div class="stepdiv">
-					<div class="stepImage">
-						<img src="/application/views/images/mypage/icon_sale01.png" alt="sale_icon_image" class="icon_sale01" id="icon_sale01">
-						<!-- <img src="/application/views/images/mypage/icon_sale01g.png" alt="sale_icon_image" class="icon_sale01g" id="icon_sale01g"> -->
-						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow01" id="icon_arrow01">
-						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow01g" id="icon_arrow01g"> -->
-
-						<img src="/application/views/images/mypage/icon_sale02.png" alt="sale_icon_image" class="icon_sale02" id="icon_sale02">
-						<!-- <img src="/application/views/images/mypage/icon_sale02g.png" alt="sale_icon_image" class="icon_sale02g" id="icon_sale02g"> -->
-						<img src="/application/views/images/mypage/icon_arrow01.png" alt="sale_icon_image" class="icon_arrow02" id="icon_arrow02">
-						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow02g" id="icon_arrow02g"> -->
-
-						<img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03" id="icon_sale03">
-						<!-- <img src="/application/views/images/mypage/icon_sale03g.png" alt="sale_icon_image" class="icon_sale03g" id="icon_sale03g"> -->
-						<img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03" id="icon_arrow03g">
-						<!-- <img src="/application/views/images/mypage/icon_arrow01g.png" alt="sale_icon_image" class="icon_arrow03g" id="icon_arrow03g"> -->
-
-						<img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04" id="icon_sale04">
-						<!-- <img src="/application/views/images/mypage/icon_sale04g.png" alt="sale_icon_image" class="icon_sale04g" id="icon_sale04g"> -->
-					</div><!-- stepImage 닫음 -->
-					<div class="stepText">
-						<span class="step1">咨询中</span>
-						<span class="step2">预定成功</span>
-						<span class="step3">服务结束</span>
-						<span class="step4">已付款</span>
-					</div>
-
-					<div class="absoluteDiv">
-						<span>价格</span><br>
-						<span class="font_blue">1500元</span><br>
-						<span class="btn btn-info">取消交易</span>
-					</div>
-				</div><!-- stepdiv 닫음 -->
-			</div><!-- salesList 닫음, 이 박스가 반복됨-->
-
-			<nav class="paginationNav">
-				<ul class="pagination pagination-sm" id="pmPagination" name="pmPagination">
-					<li>
-						<a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
-					</li>
-					<li><a href="#">1</a></li>
-					<li><a href="#">2</a></li>
-					<li><a href="#">3</a></li>
-					<li>
-					<a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
-					</li>
-				</ul>
-			</nav>
-
-			<button class="btn" id="allSelect" name="allSelect">전체선택</button>
-			<button class="btn" id="deleteSelect" name="deleteSelect">선택삭제</button>
-
-		</div><!-- profileWrap1 end -->
+		</div><!-- hostWrap 닫음 -->
