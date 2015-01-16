5bc350c4a8d29eae53083e1378978a1f605b7c39
sigurae723 <sbs@linkasia.co.kr>
2015-01-16 10:55:53 +0900

    Sprint_0010_CompanyIntro
    메뉴용 php 생성

---
+++ application/views/companyinfo/companymenu.php
@@ -0,0 +1,93 @@
+<div id="guestWrap">
+	<aside id="leftmenuWrap">
+		<span class="list-group-item active">Company Menu</span></span>
+		<span class="list-group-item" id='guestHome'><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;&nbsp;1 Home</span>
+		<span class="list-group-item" id='guestBucket'><span class="glyphicon glyphicon-hand-up" aria-hidden="true"></span>&nbsp;&nbsp;2 List</span>
+		<span class="list-group-item" id='guestReservation'><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;&nbsp;3 List&nbsp;&nbsp;</span>
+		<span class="list-group-item" id='guestBuy'><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;&nbsp;4 List&nbsp;&nbsp;</span>
+		<span class="list-group-item" id='guestCancel'><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>&nbsp;&nbsp;5 &nbsp;&nbsp;</span>
+	</aside><!-- leftmenuWrap end -->
+
+
+	<div id="rightWrap">
+		<? $data = "";
+			$this->load->view("/mypage/m_guest0",$data);
+		?>
+	</div><!-- rightWrap end -->
+</div><!-- guestWrap end -->
+
+
+
+<script type="text/javascript">
+	$(function(){
+
+			$('#guestHome').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/mypage/myPage_M/myGuestHome",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#guestBucket').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/mypage/myPage_M/myGuestAdmin",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#guestReservation').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/mypage/myPage_M/myGuestItemAdmin",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#guestBuy').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/mypage/myPage_M/myGuestSalesAdmin",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#guestCancel').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/mypage/myPage_M/myGuestCancelAdmin",
+					success: function (data){
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+	});
+</script>
