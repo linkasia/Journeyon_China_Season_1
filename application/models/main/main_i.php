ac995d76a0de8955a7e1bfa6b8f9a6d852d5820b
hyunchoul0727 <hhc@linkasia.co.kr>
2015-01-20 13:42:34 +0900

    Back-End #71 Bug_Mypage_pictures_query
    메인 쿼리 수정

--- application/models/main/main_i.php
+++ application/models/main/main_i.php
@@ -62,21 +62,24 @@
 		//최근 올라온 3개의 도시
 		function Travel(){
 
-			$sql ="SELECT b.email,
-											b.face_img_path,
-											c.code_nm,
-											c.ref1,
-											a.*,
-											(SELECT img_path
-											   FROM spot
-											   LIMIT 1
-											 ) AS image
-							FROM product a
-							LEFT JOIN USER b ON a.user_num = b.user_num
-							LEFT JOIN country_table c ON b.mother_area_code = c.class AND b.mother_country_code = c.code
-							WHERE a.useYn='Y'
-							ORDER BY a.create_date DESC
-							LIMIT 3";
+			$sql ="SELECT u.*,
+											z.img_path as image
+							FROM(
+											SELECT b.email,
+															b.face_img_path,
+															c.code_nm,
+															c.ref1,
+															a.*
+											FROM product a
+											LEFT JOIN USER b ON a.user_num = b.user_num
+											LEFT JOIN country_table c ON b.mother_area_code = c.class AND b.mother_country_code = c.code
+											WHERE a.useYn='Y'
+											AND a.adminYn='Y'
+											ORDER BY a.create_date DESC
+											LIMIT 3
+							) AS u
+							LEFT JOIN spot z ON u.product_num = z.product_num
+							GROUP BY product_num";
 			$query = $this->db->query($sql);
 			$result = $query->result();
 			return $result;
@@ -85,22 +88,24 @@
 		//관리자가 선택한 6개의 도시
 		function Admin_choice(){
 
-			$sql ="SELECT b.email,
-											b.face_img_path,
-											c.code_nm,
-											c.ref1,
-											a.*,
-											(SELECT img_path
-											   FROM spot
-											   LIMIT 1
-											 ) AS image
-							FROM product a
-							LEFT JOIN USER b ON a.user_num = b.user_num
-							LEFT JOIN country_table c ON b.mother_area_code = c.class AND b.mother_country_code = c.code
-							WHERE a.useYn='Y'
-							AND a.adminYn='Y'
-							ORDER BY a.create_date DESC
-							LIMIT 6";
+			$sql ="SELECT u.*,
+											z.img_path as image
+							FROM(
+											SELECT b.email,
+															b.face_img_path,
+															c.code_nm,
+															c.ref1,
+															a.*
+											FROM product a
+											LEFT JOIN USER b ON a.user_num = b.user_num
+											LEFT JOIN country_table c ON b.mother_area_code = c.class AND b.mother_country_code = c.code
+											WHERE a.useYn='Y'
+											AND a.adminYn='Y'
+											ORDER BY a.create_date DESC
+											LIMIT 6
+							) AS u
+							LEFT JOIN spot z ON u.product_num = z.product_num
+							GROUP BY product_num";
 			$query = $this->db->query($sql);
 			$result = $query->result();
 			return $result;
