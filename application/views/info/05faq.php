e72f131569f8cf30b73c4095757bd7ff6eaf2de5
sigurae723 <sbs@linkasia.co.kr>
2015-01-21 13:15:03 +0900

    Sprint_0010_CompanyIntro FAQ
    Info탭 - FAQ
    하드코딩 컨텐츠 업로드

--- application/views/info/05faq.php
+++ application/views/info/05faq.php
@@ -2,4 +2,203 @@
 	<div class="underline">疑问解答</div>
 </div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
 
-05faq
+<div class="bs-example">
+    <div class="panel-group" id="accordion">
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">1. V认证是什么？</a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in">
+                <div class="panel-body">
+                    <p>个人认证是对个人用户真实身份的确认。确认来源包括其他认证用户的证明或者相关材料证明。所有的认证服务是免费的。</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">2. 为什么需要V认证？</a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>得到认证，用户易于被识辨。发布的内容更权威，提高可信度，有利于交易双方。</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">3. V认证的标准是什么？</a>
+                </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. </p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">4. 修改头像/个人资料会导致认证消失吗？ </a>
+                </h4>
+            </div>
+            <div id="collapseFour" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>得到认证需要满足以下条件：<br>
+							绑定手机， 有头像。。。<br>
+							申请认证填写资料时，如果实际生日跟身份证生日不一致，以身份证号码为准。<br>
+					</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">5. 认证的身份是永久的吗？</a>
+                </h4>
+            </div>
+            <div id="collapseFive" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>...</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix">6. 如何同步卓尼JOURNEY ON!到其他社交网站？</a>
+                </h4>
+            </div>
+            <div id="collapseSix" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>在首页顶部“登录“，点击您要绑定帐号的博客名进入相应的博客页面，然后点击右侧栏的“博客设置”，在“同步”一栏下设置绑定各个社交帐号，目前支持新浪微博 、腾讯微博、豆瓣、Facebook、Instagram等等。</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">7. 卓尼JOURNEY ON!服务会否收费？</a>
+                </h4>
+            </div>
+            <div id="collapseSeven" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>仅收取客人服务费的10%作为服务佣金</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseEight">8.（Guest）如何预订导游或个性化旅行服务?</a>
+                </h4>
+            </div>
+            <div id="collapseEight" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>1）卓尼JOURNEY ON!上的导游都按照兴趣和专业分了类，旅行者根据自己的需要找到适合自己的导游。<br>
+						2）以网站直接对话、微信、微博、手机客户端、电子邮件等5种通讯方式，联系并确认导游的档期。<br>
+						3）确认行程单和需要支付的金额，回复确认。<br>
+						4）支付相关费用，支付成功后，你的卓尼JOURNEY ON!个人页面的选栏上，找到并可以查询到您当前的账单和支付状态。<br>
+					</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseNine">9. 在行程中遇上问题（旅客/导游不出现；发生争执该怎么办？</a>
+                </h4>
+            </div>
+            <div id="collapseNine" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>...</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTen">10. 如何对微导游的服务作评价？</a>
+                </h4>
+            </div>
+            <div id="collapseTen" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>每次行程结束后，你可以发布对服务导游的评价、上传您的旅行日志。让其他的旅行者日后能做更明智的选择。</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">11. (Guide)如何发布导游、租车、旅行线路等服务?</a>
+                </h4>
+            </div>
+            <div id="collapseEleven" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>每次行程结束后，你可以发布对服务导游的评价、上传您的旅行日志。让其他的旅行者日后能做更明智的选择。</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwelve">12. 保证金退还规则：</a>
+                </h4>
+            </div>
+            <div id="collapseTwelve" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>...</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseThirteen">13. 如何删除我撰写的服务/发表内容？</a>
+                </h4>
+            </div>
+            <div id="collapseThirteen" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>前往您想删除的内容，点击编辑，然后点击删除。请紧记，删除后，该执行动作将无法还原。（您只可删除自己建立的内容。）</p>
+                </div>
+            </div>
+        </div>
+
+		<div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFourteen">14. 什么情况会导致账号被封锁？</a>
+                </h4>
+            </div>
+            <div id="collapseFourteen" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>我们将禁止任何人士进行可能会令人感到厌恶或滥用的行为，例如：上载/发放不当的内容，包括色情，暴力等等；发放垃圾讯息或对别人构成骚扰。 如帐户被停用/封锁，您仍可登入至</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<style type="text/css">
+    .bs-example{
+    	margin: 20px; height: 800px;
+    }
+</style>
