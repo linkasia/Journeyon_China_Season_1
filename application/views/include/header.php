28da40268ef25b8b1ca0e929ebaecf28f4018f08
Shin Beom Seok <sbs@linkasia.co.kr>
2015-01-30 12:28:15 +0900

    Script ADD
    Header에 스크립트 추가

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -66,6 +66,17 @@ $maincontents="当地人推荐的最佳自由行之地 Journey On";
 		<script src="/application/views/js/simple-modal.js" type="text/javascript" charset="utf-8"></script>
 		<!-- script src="/application/views/js/demo.js" type="text/javascript" charset="utf-8"></script -->
 
+		<!-- 1/30 추가 스크립트 -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-59164921-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
+
 	</head>
 
 
