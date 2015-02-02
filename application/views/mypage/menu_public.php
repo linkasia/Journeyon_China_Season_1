705c8b08ad17c0dece37078c835927e82cc329d4
hyunchoul0727 <hhc@linkasia.co.kr>
2015-02-02 16:33:56 +0900

    Bug #87 프로필 사진 업로드 불가
    2015.02.02
    이미지 업로드
    롤백된 내용 다시 업로드

--- application/views/mypage/menu_public.php
+++ application/views/mypage/menu_public.php
@@ -1,12 +1,13 @@
 <div id="mypublicWrap">
-
+<form name="profileUploadfrm" id="profileUploadfrm" method='post' enctype="multipart/form-data" action="/index.php/mypage/mypage_M/profileModify">
+	<?foreach($user as $v){?>
 	<!-- 왼쪽 프로필 사진 영역 -->
 	<aside id="leftAside">
 		<div class="profileImgDiv">
 			<div class="bandDiv">
 				<span>Jeon Hyo Sung</span>
 			</div>
-			<img src="/application/views/images/main/profile02.jpg" alt="profile" class="profileImg" id="profileImg">
+			<img src="<?=$v->face_img_path?>" alt="profile" class="profileImg" id="leftProfileImg">
 		</div>
 		<div class="profileTxt">
 			<p>From Korea</p>
@@ -19,21 +20,31 @@
 
 
 	<!-- 여기서부터 오른쪽 박스 -->
-	<form name="profileUploadfrm" id="profileUploadfrm" method='post' enctype="multipart/form-data" action="/index.php/mypage/mypage_M/profileModify">
-	<?foreach($user as $v){?>
+
 	<div id="rightDiv">
 		<div id="introduceSelf">
 			<p class="headline">Introduce Your Self</p>
 			<p class="subheadline">This is pre-chat with your members</p>
 			<!-- <button class="btn btn-primary" id="editProfile">Edit your profile</button> -->
 		</div>
+
 		<div id="profileQuestion">
 			<button type="submit" class="btn btn-info" id="saveProfile" name="saveProfile">Save</button>
 			<button class="btn btn-warning" id="cancelSave">Cancel</button>
 			<div class="profilepicAdd" id="profilepicAdd">
 				<span class="glyphicon glyphicon-plus plus"></span>
-				<img src="/application/views/images/main/profile02.jpg" alt="profile" class="uploadImg" id="profileImg">
+				<img src="<?=$v->face_img_path?>" alt="" class="uploadImg" id="profileImg">
 			</div>
+
+			<div class="name1">
+				<div class="leftSection">
+					<p class="rightAlign">What is your Name?</p>
+				</div>
+				<div class="rightSection">
+						<input type="file" class="form-control2 text" id="inputImage"  name="inputImage" value="<?=$v->face_img_path?>">
+				</div>
+			</div>
+
 			<div class="name1">
 				<div class="leftSection">
 					<p class="rightAlign">What is your Name?</p>
@@ -234,6 +245,9 @@ $(function(){
 	$(function(){
 		$('#profileUploadfrm').ajaxForm({
 			success: function(data){
+				alert(data);
+				$("#profileImg").attr("src",data);
+				$("#leftProfileImg").attr("src",data);
 				alert("수정되었습니다.");
 			}
 		});
