15b88555bcc56da18b20d028d822e52fea08456c
Shin Beom Seok <sbs@linkasia.co.kr>
2015-02-09 16:12:34 +0900

    Front-End #127 파일생성
    관리자 페이지 밑 작�

---
+++ application/views/admin/adminmenu.php
@@ -0,0 +1,86 @@
+<div id="companyInfoWrap">
+	<aside id="leftmenuWrap">
+		<span class="list-group-item active" id='adminMain'>Main</span>
+		<span class="list-group-item" id='adminMember'>Member</span>
+		<span class="list-group-item" id='adminProduct'>Product</span>
+		<span class="list-group-item" id='adminBook'>Book</span>
+		<span class="list-group-item" id='adminBook'>Notice</span>
+		<span class="list-group-item" id='adminBook'>Board</span>
+	</aside><!-- leftmenuWrap end -->
+</div>
+
+
+<!--
+<script type="text/javascript">
+	$(function(){
+
+			$('#infoMicrotravel').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/info/infomenu/infoMicrotravel",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#infoHowhost').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/info/infomenu/infoHowhost",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#infoHowTraveler').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/info/infomenu/infoHowTraveler",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#infoPrivacy').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/info/infomenu/infoPrivacy",
+					success: function (data){
+						//alert(data);
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+
+			$('#infoFaq').click( function(){
+				$.ajax({
+					type:"POST" ,
+					dataType:"text",
+					contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+					data:{ },
+					url:"/index.php/info/infomenu/infoFaq",
+					success: function (data){
+						document.getElementById('rightWrap').innerHTML =data;
+					}
+				});
+			});
+	});
+</script> -->
