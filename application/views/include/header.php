bd790ba05868b5bfb3f447270d78d4b891622c08
hyunchoul0727 <hhc@linkasia.co.kr>
2015-02-13 17:32:30 +0900

    2015.02.13
    프로필 에러수정

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -118,7 +118,7 @@ $maincontents="当地人推荐的最佳自由行之地 Journey On";
 								<img src="<?=$this->session->userdata['face_img_path']?>" alt="myprofile_Picture" class="img-circle dropdown-toggle" type="button" id="profileMenu" data-toggle="dropdown" aria-expanded="true"><span class="caret caret-block" data-toggle="dropdown" aria-expanded="true"></span>
 								<!-- <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true"></button> -->
 								<ul class="dropdown-menu" role="menu" aria-labelledby="profileMenu">
-									<li role="presentation"><a role="menuitem" tabindex="-1" href="/index.php/mypage/myPage_M/mypublic">个人页面</a></li>
+									<li role="presentation"><a role="menuitem" tabindex="-1" href="/index.php/mypage/myPage_M/mypublic?userNum=<?=$this->session->userdata['num']?>">个人页面</a></li>
 									<li role="presentation"><a role="menuitem" tabindex="-1" href="/index.php/mypage/myPage_M/myaccountSetting">编辑及设定</a></li>
 									<li role="presentation"><a role="menuitem" tabindex="-1" href="/index.php/mypage/myPage_M/myguide">微导游</a></li>
 									<li role="presentation"><a role="menuitem" tabindex="-1" href="/index.php/mypage/myPage_M/myguest?mode=0">旅行者</a></li>
