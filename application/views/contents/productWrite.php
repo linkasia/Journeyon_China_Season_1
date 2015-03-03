c4fe4802c169b4a5f923056eb62305299630252b
Shin Beom Seok <sbs@linkasia.co.kr>
2015-03-03 17:02:00 +0900

    Front Fix 2015.03.03
    일부 변경

--- application/views/contents/productWrite.php
+++ application/views/contents/productWrite.php
@@ -68,7 +68,7 @@
 								</div-->
 									<div id="UploadPreviewDiv"><img src="" alt="" id="UploadPreviewImg"></div>
 									<input type="file" class="filestyle btn btn-info" id="fileUpload" name="fileUpload" data-icon="false" placeholder="파일 업로드">
-									<!--span id="posa_pictureupload" name="fileUpload">Upload</span -->
+									<span id="posa_pictureupload" name="fileUpload">Upload</span>
 									<!-- button class="btn btn-default" id="posa_imagecheckbtn" >Image Check</button -->
 									<p class="txt_blue captionTitle">Picture Caption - Please introduce your picture</p>
 									<input type='hidden' name="hiddenId" id="hiddenId" class="form-control" value="">
