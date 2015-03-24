ebc6701333922a20144565075b318550b156a40f
Shin Beom Seok <sbs@linkasia.co.kr>
2015-03-24 17:30:51 +0900

    FF 2015.03.24
    이미지추가
    버킷리스트쪽 작업

--- application/views/mypage/m_guest1.php
+++ application/views/mypage/m_guest1.php
@@ -4,26 +4,24 @@
 				<div class="underline">我的收藏</div>
 			</div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
 
-
-			<div id="selectDiv">
+			<div id="selectDiv" class="bd">
 				<?foreach($bucketList as $v){?>
 				<div id="somenailwrapDiv">
-					<div class="somenailDiv">
-						<img src="<?=$v->ref1?>" alt="countryImg" id="somenailImg" name="somenailImg" onclick="myBucketList('<?=$v->sortcountry?>','<?=$v->country_code?>')">
+					<div class="somenailDiv img-circle" onclick="myBucketList('<?=$v->sortcountry?>','<?=$v->country_code?>')">
+						<img src="/application/views/images/contents/01mongolia.jpg" alt="countryImg" id="somenailImg" name="somenailImg">
 						<p class="contentsNumber"><?=$v->cnt?></p>
 					</div><!-- somenailDiv end  -->
 					<div class="somenailText"><?=$v->country_nm?></div>
 				</div>
+				
 				<?}?>
 			</div>
 
 			<div id="listWrap">
-
 				<?
 					//$data['slaesBuyList'] = $this->country_M->salesCountry($scontry, $contry, null);
 					//$this -> load ->view('/mypage/productList', $data);
 				?>
-
 			</div>
 
 
