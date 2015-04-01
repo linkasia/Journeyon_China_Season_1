d033c017f4d0e6980d66ebc3fc43676e806f33b4
hyunchoul0727 <hhc@linkasia.co.kr>
2015-04-01 14:21:02 +0900

    2015.04.01
    소스 수정

--- application/views/include/headerimg.php
+++ application/views/include/headerimg.php
@@ -1,7 +1,4 @@
 <div id="header_wrap">
-<<<<<<< HEAD
 	<img src="" alt="" id='bakGround' name='bakGround'>
-=======
 	<img src="/application/views/images/main/bg06.jpg" alt="" class="innerHeaderimg">
->>>>>>> origin/Development
 </div>
