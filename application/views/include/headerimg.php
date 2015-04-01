e9360cb2c48a264c60815bbd631675976addde23
Shin Beom Seok <sbs@linkasia.co.kr>
2015-04-01 16:21:21 +0900

    FF 2015.04.01
    Merge

--- application/views/include/headerimg.php
+++ application/views/include/headerimg.php
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 <div id="header_wrap3">
 	<div class="bgimgDiv">
 		<!-- <img src="/application/views/images/main/bg08.jpg" alt="" class="innerHeaderimg"> -->
+		<!-- <img src="" alt="" id='bakGround' name='bakGround'> -->
 	</div>
-=======
-<div id="header_wrap">
-	<img src="" alt="" id='bakGround' name='bakGround'>
-	<img src="/application/views/images/main/bg06.jpg" alt="" class="innerHeaderimg">
->>>>>>> origin/Development
 </div>
+
