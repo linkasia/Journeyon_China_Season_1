673849abb831cf5867d6ffd79fd0370377819505
hyunchoul0727 <hhc@linkasia.co.kr>
2015-04-02 09:37:13 +0900

    2015.04.02

--- application/views/member/login.php
+++ application/views/member/login.php
@@ -122,33 +122,20 @@
 			}
 
 
-WB2.anyWhere(function (W) {
-
-    W.widget.connectButton({
-
-        id: "wb_connect_btn",
-
-        type: '3,2',
-
-        callback: {
-
-            login: function (o) { //登录后的回调函数
-
-                alert("login: " + o.screen_name)
-
-            },
-
-            logout: function () { //退出后的回调函数
-
-                alert('logout');
-
-            }
-
-        }
-
-    });
-
-});
+			WB2.anyWhere(function (W) {
+				W.widget.connectButton({
+					id: "wb_connect_btn",
+					type: '3,2',
+					callback: {
+						login: function (o) { //登录后的回调函数
+							alert("login: " + o.screen_name)
+						},
+						logout: function () { //退出后的回调函数
+							alert('logout');
+						}
+					}
+				});
+			});
 
 			function checkLoginState() {
 				FB.getLoginStatus(function(response) {
