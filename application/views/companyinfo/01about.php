9cc323ebac619e2674e7b48180620529ba2522b2
Shin Beom Seok <sbs@linkasia.co.kr>
2015-04-03 09:27:49 +0900

    FF 2015.04.03
    CSS Fix

--- application/views/companyinfo/01about.php
+++ application/views/companyinfo/01about.php
@@ -34,4 +34,4 @@
 <div class="title0">
 	<div class="underline">团队</div>
 </div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
-<p>...</p>
+<div class="minheight600"></div>
--- application/views/companyinfo/05contact.php
+++ application/views/companyinfo/05contact.php
@@ -1,12 +1,15 @@
 <div class="title2">
 	<div class="underline">联系方式</div>
 </div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
+<div class="minheight600">
 05contact
-
 <br><br><br>
+</div>
+
 
 <div class="title2">
 	<div class="underline">合作伙伴</div>
 </div>  <!-- 라인 들어가는 타이틀 city부분 복제 -->
-
+<div class="minheight600">
 06becomepartner
+</div>
